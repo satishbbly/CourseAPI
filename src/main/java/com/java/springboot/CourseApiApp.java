dd11f8672f8ff7fda1ea08c9462a74d89c9aef9d
satishbbly <53127872+satishbbly@users.noreply.github.com>
2019-10-18 00:59:48 +0530

    Update CourseApiApp.java

--- src/main/java/com/java/springboot/CourseApiApp.java
+++ src/main/java/com/java/springboot/CourseApiApp.java
@@ -2,7 +2,7 @@ package com.java.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this is the main class
 @SpringBootApplication
 public class CourseApiApp {
 
