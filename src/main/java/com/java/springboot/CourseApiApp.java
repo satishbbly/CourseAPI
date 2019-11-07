03872901071a0c24827bd05646f99aae95a997c3
Satish Kumar <satishbbly@gmail.com>
2019-11-07 21:02:04 +0530

    pom.xml

--- src/main/java/com/java/springboot/CourseApiApp.java
+++ src/main/java/com/java/springboot/CourseApiApp.java
@@ -2,8 +2,10 @@ package com.java.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 // this is the main class
 @SpringBootApplication
+//@EnableEurekaClient
 public class CourseApiApp {
 
 	public static void main(String[] args) {
